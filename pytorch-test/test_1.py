e39f4cd1d528f806fb27a08fea38fd01e845d78c
weixing <weixing_R@163.com>
2019-12-05 15:41:23 +0800

    mod

---
+++ pytorch-test/test_1.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+import torch
+import numpy as np
+
+x = torch.empty(5, 3)
+print(x)
+
+x = torch.rand(5, 3)
+print(x)
+
+x = torch.zeros(5, 3, dtype=torch.long)
+print(x)
+
+x = torch.tensor([5.5, 3])
+print(x)
+x = torch.tensor([[5.5, 3], [5.3, 2]])
+print(x)
+
+x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
+print(x)
+
+x = torch.randn_like(x, dtype=torch.float)    # override dtype!
+print(x)                                      # result has the same size
+
+print(x.size())
+
+y = torch.rand(5, 3)
+print(x + y)
+
+print(torch.add(x, y))
+
+result = torch.empty(5, 3)
+torch.add(x, y, out=result)
+print(result)
+
+y.add_(x)
+print(y)
+
+print(x[:, 1])
+
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
+print(x.size(), y.size(), z.size())
+
+x = torch.randn(1)
+print(x)
+print(x.item())
+
+a = torch.ones(5)
+print(a)
+b = a.numpy()
+print(b)
+a.add_(1)
+print(a)
+print(b)
+
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add(a, 1, out=a)
+print(a)
+print(b)
+
+# let us run this cell only if CUDA is available
+# We will use ``torch.device`` objects to move tensors in and out of GPU
+print(torch.cuda.is_available())
+if torch.cuda.is_available():
+    device = torch.device("cuda")          # a CUDA device object
+    y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
+    x = x.to(device)                       # or just use strings ``.to("cuda")``
+    z = x + y
+    print(z)
+    print(z.to("cpu", torch.double))       # ``.to`` can also change dtype together!
