65354b895ec81b8f8c7651f3903f9ea6b56d6b71
weixing <weixing@jiankangyouyi.com>
2020-12-11 11:24:58 +0800

    python base

---
+++ 00-python-base/18_std_lib.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import shutil
+import glob
+import re
+import math
+import urllib
+import random
+from datetime import date
+import zlib
+from timeit import Timer
+import unittest
